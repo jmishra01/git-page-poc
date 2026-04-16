3847bf0c8775943b94181d5dfe3299172fe9a7ee
Jitendra Mishra <jitendramishra.jmishra@gmail.com>
2026-04-16 08:46:32 +0530

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
-
+from {vitPreprocess} from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: viteProcess(),
 	kit: {
 		adapter: adapter(),
     pages: 'build',
