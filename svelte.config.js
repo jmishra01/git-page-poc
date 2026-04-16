634985d5d095d657c993c70289047bb6d27c0611
jitendramishra <jitendra29mishra@gmail.com>
2026-04-16 08:35:43 +0530

    change config.js file

--- svelte.config.js
+++ svelte.config.js
@@ -2,11 +2,16 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	compilerOptions: {
-		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
-	},
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+    pages: 'build',
+    assets: 'build',
+    fallback: null,
+    precompress: false,
+    strict: true,
+    paths: {
+      base: dev ? '' : '/git-page-poc',
+    },
 	}
 };
 
